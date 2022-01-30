1b067c71f74c1a98ed0cd0d87db0fa2aa7ebc9fd
황지영 <ghkd6385@gmail.com>
2022-01-30 19:50:24 +0900

    commit to file-remake

---
+++ project_Stuco/js/includeHTML.js
@@ -0,0 +1,23 @@
+"use strict;"
+
+//반복 modal html file calling
+
+window.addEventListener('load', function(){
+    let allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el){
+        
+        let includePath = el.dataset.includePath;
+
+        if(includePath) {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+
+                if(this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
---
+++ project_Stuco/js/modal-popup.js
@@ -0,0 +1,4 @@
+"use strict;"
+
+//popup-style
+
