46376818cbfe07bd71ded2a60d5e5593ae3a9683
황지영 <ghkd6385@gmail.com>
2022-02-10 05:16:44 +0900

    no message

--- project_Stuco/js/chart.js
+++ project_Stuco/js/chart.js
@@ -1,29 +1,29 @@
 
 const dataset = {
-    label: '많이 쓰는 프로그램언어',
-    backgroundColor: ['#ffd950','#02bc77','#28c3d7','#ff6384'], //label color
+    title: '많이 쓰는 프로그램언어',
+    backgroundColor: ['#2B82F6 ','#1E2A64','#5D4F8D','#78CFF1'], //label color
+    borderColor: '#f8f9fa',
     data: [50,45,75,40]
 }
 const labels = ['JavaScript','Java','HTML/CSS','DataBase'];
 const datasets = { datasets:[dataset], labels:labels }
 
 const config = {
-    type: 'pie',
+    type: 'doughnut',
     data: datasets,
     options: {
         responsive: true,
         maintainAspectRatio: false,
-        legend: {
-            position: 'top',
-            fontColor: 'black',
-            align: 'center',
-            display: 'true',
-            fullWidth: true,
-            labels: {
-                fontColor:'rgb(0,0,0)'
-            },
-        },
         Plugins:{
+            legend: {
+                fontColor: 'black',
+                align: 'center',
+                display: 'true',
+                fullWidth: true,
+                labels: {
+                    fontColor:'rgb(0,0,0)',
+                },
+            },
             labels:{
                 render: 'value',
                 fontColor: 'black',
@@ -35,5 +35,16 @@ const config = {
     }
 }
 
+const actions = [
+    {
+        name: 'Position: bottom',
+        handler(chart) {
+          chart.options.plugins.legend.position = 'bottom';
+          chart.update();
+        }
+    },
+];
+
 const canvas = document.getElementById('myChartLang');
 const myChartLang = new Chart(canvas,config);
+
